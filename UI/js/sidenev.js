2fbc509362f31b62d8a51209118671122759c765
Shania Tzang <shaniatzang@gmail.com>
2018-05-05 23:46:59 +0200

    Adding and tracking the website

---
+++ UI/js/sidenev.js
@@ -0,0 +1,18 @@
+// Script to open and close sidenav
+function w3_open() {
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
