edfcfe978ab146d38011bfef6c69c97d0e9cb20e
Andreas <otriggad@gmail.com>
2018-05-16 17:36:18 +0200

    template rfcomm server that runs motors on recieved msg

---
+++ ev3/rfcomm-server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# file: rfcomm-server.py
+# auth: Albert Huang <albert@csail.mit.edu>
+# desc: simple demonstration of a server application that uses RFCOMM sockets
+#
+# $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
+
+from bluetooth import *
+import ev3dev.ev3 as ev3
+import time
+import sys
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service( server_sock, "SampleServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ],
+#                   protocols = [ OBEX_UUID ]
+                    )
+
+print("Waiting for connection on RFCOMM channel %d" % port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from ", client_info)
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if len(data) == 0: break
+        print("received [%s]" % data)
+# Run motor on received message
+        m = ev3.LargeMotor('outD')
+        n = ev3.LargeMotor('outA')
+        m.run_timed(time_sp=100, speed_sp=-100)
+except IOError:
+    pass
+
+print("disconnected")
+
+client_sock.close()
+server_sock.close()
